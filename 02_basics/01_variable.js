2d11222eeca4d9aeb6118126374a1d17bbda4631
Prabhatkumar8252 <121727522+Prabhatkumar8252@users.noreply.github.com>
2025-06-01 13:47:11 +0000

    container config done

---
+++ 02_basics/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 1444558
+let accountEmail = "kumarprabhat37299@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+
+accountEmail ="hc@hc.com"
+accountPassword ="234666"
+accountCity ="puri"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
