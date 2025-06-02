65b0899a78e107cf3574b4af513f5155e2403ab8
Prabhat kumar <121727522+Prabhatkumar8252@users.noreply.github.com>
2025-06-02 18:04:28 +0000

    conversion

---
+++ 02_basics/03_conversion.js
@@ -0,0 +1,14 @@
+let score = "65abs" // true "name" null undefine
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+// null => 0;
+// "sghthk" => string
