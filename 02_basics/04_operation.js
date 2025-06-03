b408bef6b3b058fc81de42d8cb5e65218f3355c8
Prabhat kumar <121727522+Prabhatkumar8252@users.noreply.github.com>
2025-06-03 14:20:18 +0000

    operation

--- 02_basics/04_operation.js
+++ 02_basics/04_operation.js
@@ -2,4 +2,32 @@ let value = 7
 let negValue = -value
 console.log(negValue);
 
-//
+console.log(2+2);
+console.log(2**2);
+console.log(2*2);
+console.log(2%2);
+console.log(2/2);
+console.log(2-2);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+console.log(1 + "2" + 1);
+console.log(1 + "2" + "8");
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
